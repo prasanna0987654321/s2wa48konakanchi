35b68901ee3f3e61168f86c486fdc81329d693d4
prasanna1234567890 <prasannaspring2022@gmail.com>
2022-03-29 01:43:58 -0500

    computation

--- computation.js
+++ computation.js
@@ -7,13 +7,16 @@ router.get('/', function(req, res, next) {
       const url = new URL(fake_url)
       const search_params = url.searchParams
 
-    
       storeValue=Number(search_params.get("x"))
    
     if(storeValue == 0)
     storeValue=Math.random()
-    res.write('Math.fround applied to '+storeValue+" is "+Math.fround(storeValue)+"\n");
-    res.end('Math.round applied to '+storeValue+" is "+Math.round(storeValue));
+    res.write('Math.abs applied to '+storeValue+" is "+Math.abs(storeValue)+"\n");
+    res.write('Math.acos applied to '+storeValue+" is "+Math.acos(storeValue)+"\n");
+    res.write('Math.sin applied to '+storeValue+" is "+Math.sin(storeValue)+"\n");
+    res.end('Math.sinh applied to '+storeValue+" is "+Math.sinh(storeValue));
+
+
  });
 
 module.exports = router;
